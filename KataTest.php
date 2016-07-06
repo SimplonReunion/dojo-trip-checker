b27e2b3a2276e4d03244cedb8a54f80bd50115a2
PublicVar <dev.publicvar@gmail.com>
2016-07-06 10:08:00 +0400

    Fix typo

--- KataTest.php
+++ KataTest.php
@@ -4,30 +4,29 @@ require 'kata.php';
 
 class KataTest extends \PHPUnit_Framework_TestCase
 {
-
     protected $stations;
     protected $links;
 
-    public function setUp(){
-      $this->stations = ["ADL", "MEL", "SYD", "BRI"];
-      $this->links =  ["ADL" => ["MEL"], "MEL" => ["ADL", "SYD"],  "SYD" => ["BRI"]];
+    public function setUp()
+    {
+        $this->stations = ["ADL", "MEL", "SYD", "BRI"];
+        $this->links =  ["ADL" => ["MEL"], "MEL" => ["ADL", "SYD"],  "SYD" => ["BRI"]];
     }
 
     public function testTripPossibleStartToEnd()
     {
-        $this->assertEquals("Possible", check_trip( "ADL", "BRI", $this->stations, $this->links));
+        $this->assertEquals("Possible", check_trip("ADL", "BRI", $this->stations, $this->links));
     }
 
     public function testTripPossibleAStationToEnd()
     {
         $this->markTestSkipped();
-        $this->assertEquals("Possible", check_trip( "MEL", "BRI", $this->stations, $this->links));
+        $this->assertEquals("Possible", check_trip("MEL", "BRI", $this->stations, $this->links));
     }
 
     public function testTripImpossible()
     {
         $this->markTestSkipped();
-        $this->assertEquals("Impossible", check_trip( "SYD", "ADL", $this->stations, $this->links));
+        $this->assertEquals("Impossible", check_trip("SYD", "ADL", $this->stations, $this->links));
     }
-
 }
